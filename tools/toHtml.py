fab73546e070208ec473c8e0897a0108c4647297
dk-study9900 <128661778+dk-study9900@users.noreply.github.com>
2025-05-04 23:00:04 -0500

    generated html

--- tools/toHtml.py
+++ tools/toHtml.py
@@ -3,7 +3,7 @@ from nbformat import read
 import os
 
 # Load the notebook
-with open("D:\\Downloads\\Lesson5_CaseStudy.ipynb", "r", encoding="utf-8") as f:
+with open("D:\\0-AI-ML\\ai-ml\\CaseStudy\\FoodHub\\Learner_Notebook_Full_Code.ipynb", "r", encoding="utf-8") as f:
     notebook = read(f, as_version=4)
 
 # Convert to HTML
@@ -11,5 +11,5 @@ html_exporter = HTMLExporter()
 html_data, _ = html_exporter.from_notebook_node(notebook)
 
 # Save the HTML file
-with open("D:\\Downloads\\Lesson5_CaseStudy.html", "w", encoding="utf-8") as f:
+with open("D:\\0-AI-ML\\ai-ml\\CaseStudy\\FoodHub\\Learner_Notebook_Full_Code.html", "w", encoding="utf-8") as f:
     f.write(html_data)
